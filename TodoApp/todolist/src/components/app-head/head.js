573fe565c522359d774782eaa36ca1e9c4d2e3a8
Trần Thăng <ngocthangtrantb@gmail.com>
2021-04-19 09:31:37 +0700

    split component - custom folder

---
+++ TodoApp/todolist/src/components/app-head/head.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './head.css';
+
+class head extends Component {
+    render() {
+        return (
+            <div id='top'>
+                <h1>Todo list</h1>
+            </div>
+        );
+    }
+}
+
+export default head;
