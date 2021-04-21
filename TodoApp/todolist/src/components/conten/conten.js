adc554c32fdc9f618d7258b8d062d2f7a9012af6
Trần Thăng <ngocthangtrantb@gmail.com>
2021-04-21 15:04:34 +0700

    item components

--- TodoApp/todolist/src/components/conten/conten.js
+++ TodoApp/todolist/src/components/conten/conten.js
@@ -1,118 +1,67 @@
 import React, { Component } from 'react';
 import './conten.css';
 
+
+const Item = (props)=>{
+    const {title, conten} = props;
+    return(
+        <div className='item'>
+            <h3>{title}</h3>
+            <span>
+                {conten}
+            </span>
+        </div>
+    )
+}
+
 class conten extends Component {
+    constructor(){
+        super();
+        this.state={
+            conten:[
+                {
+                    title:"Title",
+                    conten:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!"
+                },
+                {
+                    title:"Title",
+                    conten:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!"
+                },
+                {
+                    title:"Title",
+                    conten:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!"
+                },
+                {
+                    title:"Title",
+                    conten:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!"
+                },
+                {
+                    title:"Title",
+                    conten:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!"
+                },
+                {
+                    title:"Title",
+                    conten:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!"
+                },
+                {
+                    title:"Title",
+                    conten:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!"
+                }
+            ]
+        }
+    }
     render() {
+        const {conten} = this.state;
         return (
             <div className='conten'>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
-                <div class="item">
-                    <h3>Title</h3>
-                    <span>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ullam dolor ad alias nostrum facilis assumenda molestias? Dolore nobis a placeat consequuntur voluptate voluptates illo sed. Quos quis voluptates necessitatibus!
-                </span>
-                </div>
+                {
+                    !conten.length && <div>No conten</div>
+                }
+                {
+                    conten.length && conten.map((item, index)=>{
+                        return(<Item title={item.title} conten={item.conten} key={index} />)
+                    })
+                }
             </div>
         );
     }
