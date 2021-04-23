e12b3909ab56659a217fbb0d6aa3a19e9ee8dfe9
Trần Thăng <ngocthangtrantb@gmail.com>
2021-04-24 01:07:15 +0700

    Add animation to the component switch function

--- TodoApp/todolist/src/App.js
+++ TodoApp/todolist/src/App.js
@@ -1,8 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import { useTrail, a } from '@react-spring/web'
 import Head from './components/app-head/head';
 import Plusbtn from './components/btn/plus';
 import Conten from './components/conten/conten';
 import Fromconten from './components/from-conten/fromconten';
+
+const Trail=({ open, children }) => {
+    const items = React.Children.toArray(children)
+    const trail = useTrail(items.length, {
+        config: { mass: 5, tension: 2000, friction: 200 },
+        opacity: open ? 1 : 0,
+        x: open ? 0 : 20,
+        height: open ? 0 : 0,
+        from: { opacity: 0, x: 20, height: 0 },
+    })
+    return (
+        <div>
+            {trail.map(({ height, ...style }, index) => (
+                <a.div key={index} style={style}>
+                    <a.div style={{ height }}>{items[index]}</a.div>
+                </a.div>
+            ))}
+        </div>
+    )
+}
+
 class App extends Component {
     constructor() {
         super();
@@ -21,8 +43,15 @@ class App extends Component {
                     })
                 }}
                 />
-                {!this.state.clickBtn && <Conten />}
-                {this.state.clickBtn && <Fromconten />}
+                {!this.state.clickBtn &&
+                    <Trail open={true}>
+                        <Conten />
+                    </Trail>}
+                {this.state.clickBtn &&
+                    <Trail open={true}>
+                        <Fromconten />
+                    </Trail>
+                }
             </>
         );
     }
